48361371835f79930651b5ce6877e7ac45c36672
Ali El Husseini <elhusseiniali@icloud.com>
2022-08-17 15:50:29 +0200

    Define __bool__ method.

--- domain.py
+++ domain.py
@@ -17,6 +17,16 @@ class ColumnDomain():
         return input_string in self.added['must'] or \
             input_string in self.added['may']
 
+    def __bool__(self):
+        return self.original["must"] != set() \
+            and self.original["may"] != set() \
+            and self.current["must"] != set() \
+            and self.current["may"] != set()\
+            and self.added["must"] != set() \
+            and self.added["may"] != set()\
+            and self.removed["must"] != set() \
+            and self.removed["may"] != set()
+
     def __str__(self):
         return f"Original: {self.original}, Current: {self.current}, "\
                f"Added: {self.added}, Removed: {self.removed}, "\
